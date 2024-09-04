7b10eddf4bd05ecc13b6d2c6770fb456e2abd586
Luke Doty <lukedoty03@gmail.com>
2024-09-04 13:37:39 -0400

    Added sorting layers to html content

--- src/contentIndex.js
+++ src/contentIndex.js
@@ -1,3 +1,3 @@
 content.add(new ImageContent("/assets/images/picture.jpeg", 1, 125, 125, 150, 150));
 content.add(new ImageContent("/assets/images/big picture.png", 0, 100, 100, 150, 150));
-content.add(new DivContent("/assets/html/test.html", 1, -350, 0, 300, 300));
+content.add(new DivContent("/assets/html/test.html", 1, -350, 1, 300, 300));

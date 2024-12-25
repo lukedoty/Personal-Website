77c4af08e2e6b5dbbc2a750c2bb0b0dc37f8a888
Luke Doty <lukedoty@gatech.edu>
2024-12-25 01:56:54 -0500

    Changed blog folder structure

--- src/contentIndex.js
+++ src/contentIndex.js
@@ -7,6 +7,6 @@ content.add(new DivContent("./assets/html/test.html", 0, -350, 0, 300, 300));
 //content.add(new DivContent("./assets/html/watchlist.html", 0, 1000 - 200, 1000 - 300, 400, 600))\
 
 //Blog
-content.add(new DivContent("./assets/blog/12-24-24/page1.html", 0, 1224, 2024, 583, 827));
-content.add(new DivContent("./assets/blog/12-24-24/page2.html", 0, 1224 + 583 + 20, 2024, 583, 827));
-content.add(new DivContent("./assets/blog/12-25-24/page1.html", 0, 1224, 2024 + 827 + 40, 583, 827));
+content.add(new DivContent("./assets/blog/2024/dec/24/page1.html", 0, 1224, 2024, 583, 827));
+content.add(new DivContent("./assets/blog/2024/dec/24/page2.html", 0, 1224 + 583 + 20, 2024, 583, 827));
+content.add(new DivContent("./assets/blog/2024/dec/25/page1.html", 0, 1224, 2024 + 827 + 40, 583, 827));
